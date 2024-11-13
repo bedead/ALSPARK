776b85f21e3ca62877c570c13bb9d1f13cc59eca
Satyam Mishra <satyam.work.only@gmail.com>
2024-11-13 13:20:22 +0000

    added image editing canvas to sketch to realistic | backend updation required

--- AI-Sketch-to-realistic-maker/Gradio/client.py
+++ AI-Sketch-to-realistic-maker/Gradio/client.py
@@ -3,32 +3,81 @@ import requests
 from PIL import Image
 from io import BytesIO
 
-API_URL = "http://127.0.0.1:5000/generate"  # Flask runs on port 5000 by default
+API_URL = "http://127.0.0.1:5000/generate"
 
 
-def generate_realistic_image(sketch):
+def generate_realistic_image(image, prompt):
+    if image is None:
+        return gr.Error("Either upload an image or draw.")
+
     buffered = BytesIO()
-    sketch.save(buffered, format="PNG")
+    image.save(buffered, format="PNG")
     img_data = buffered.getvalue()
 
+    data = {"prompt": prompt} if prompt else {}
+
     response = requests.post(
-        API_URL,
-        files={"sketch": ("sketch.png", img_data, "image/png")},
+        API_URL, files={"sketch": ("sketch.png", img_data, "image/png")}, data=data
     )
 
     if response.status_code == 200:
         img = Image.open(BytesIO(response.content))
         return img
     else:
-        return "Error generating image!"
+        return gr.Error("Error generating image!")
 
 
-sketch_input = gr.components.Image(type="pil", label="Upload Sketch")
-output_image = gr.components.Image(label="Generated Realistic Image")
+def predict(im):
+    return im["composite"]
 
-gr.Interface(
-    fn=generate_realistic_image,
-    inputs=sketch_input,
-    outputs=output_image,
-    title="Sketch-to-Realistic Image Generator",
-).launch(share=True)
+
+def clear_all():
+    return (
+        None,
+        None,
+        "",
+        None,
+    )
+
+
+with gr.Blocks() as demo:
+    gr.Markdown("# Sketch-to-Realistic Image Generator")
+
+    with gr.Row(variant="panel", equal_height=True):
+        with gr.Column(scale=6):
+            im = gr.ImageEditor(
+                type="numpy",
+                label="Edit Image",
+                # canvas_size=(900, 500),
+                # crop_size="16:9",
+                height=700,
+            )
+
+        with gr.Column(scale=4):
+            im_preview = gr.Image(interactive=False, label="Preview")
+            # with gr.Row():
+            prompt = gr.Textbox(
+                label="Optional Prompt",
+                placeholder="Write a descriptive prompt of sketch.",
+            )
+
+            with gr.Row():
+                generate_button = gr.Button("Generate Image", variant="primary")
+                clear_btn = gr.Button("Clear", variant="secondary")
+            output_image = gr.Image(height=500, label="Generated Realistic Image")
+
+    generate_button.click(
+        generate_realistic_image,
+        inputs=[im_preview, prompt],
+        outputs=output_image,
+    )
+
+    clear_btn.click(
+        fn=clear_all,
+        inputs=None,
+        outputs=[im, im_preview, prompt, output_image],
+    )
+
+    im.change(predict, inputs=im, outputs=im_preview, show_progress="hidden")
+
+demo.launch(share=True)
